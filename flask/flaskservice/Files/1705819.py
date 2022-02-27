036918c486a604bab643661d576a0b34a590f774
Shailesh Das <shaileshdas@Shaileshs-MacBook-Air.local>
2022-02-27 17:01:47 +0530

    backend-plus-flask-merge

---
+++ flask/flaskservice/Files/1705819.py
@@ -0,0 +1,28 @@
+### TODO: A utility class which will be created by the user , with Class name as " _[your roll number]" ,
+# TODO: all transformations should be written inside a function which will be called inside the predict method
+class _1705819():
+
+    ## TODO: Please note that document id should be present till the getPredictions method
+    def __tranformation1(self,data):
+        
+        # your transformation logic goes here
+        mapper = data.groupby('customerName')['invoiceAge'].mean().to_dict()
+        data['age_invoice_y'] = data['customerName'].map(mapper)
+        return data
+
+    def __transformation2(self,data):
+
+        # your transformation logic goes here
+        return data
+
+    def getPredictions(self,data,model):
+        data = self.__tranformation1(data)
+        data = self.__transformation2(data)
+        # your feature list, column names
+        features = ['customerNumber', 'invoiceId', 'actualOpenAmount', 'invoiceAge', 'age_invoice_y']
+        print(data[features])
+        # data should be a dataFrame and not a numpy array
+        predictions = model.predict(data[features])
+        data['predictions'] = predictions
+        pred = data.loc[:,['documentNumber','predictions']].to_dict(orient="records")
+        return pred
